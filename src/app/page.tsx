f10a5a3fe768d9cd1ac0a041379cf5b8efc86b4b
abdutech <abduakmel5@gmail.com>
2025-08-03 22:35:45 +0300

    metadata added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ const Page = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      
     </div>
   )
 }
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,15 +1,33 @@
-"use client"
-import { useState,use } from "react";
-export default function Page({params}:{params:Promise<{id:string}>}) {
- const {id} = use(params);
- const [count,setCount] = useState(0)
+// "use client"
+// import { useState,use } from "react";
+// export default function Page({params}:{params:Promise<{id:string}>}) {
+//  const {id} = use(params);
+//  const [count,setCount] = useState(0)
+//   return (
+//     <div>
+//       <h1>Details page</h1>
+//       <p>This is a detail for a product with id:{id}</p>
+//       <p>Count: {count}</p>
+//       <button onClick={() => setCount(prev => prev + 1)}>+1</button>
+//     </div>
+//   );
+// }
+
+import { Metadata } from "next";
+type Props = {
+  params:Promise<{id:string}>
+}
+export const generateMetadata = async ({params}:Props) :Promise<Metadata> => {
+  const {id} = await params;
+  return {
+    title:`Product id ${id}`,
+  }
+}
+export default async function Page({params}:Props) {
+  const {id} = await params;
   return (
     <div>
-      <h1>Details page</h1>
-      <p>This is a detail for a product with id:{id}</p>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(prev => prev + 1)}>+1</button>
+      <h1>Product with Product id {id}</h1>
     </div>
   );
 }
-
